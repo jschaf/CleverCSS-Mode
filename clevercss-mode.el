28c98fe39b40de0c8544a581d6832bec8b29391a
Joe <joe@joe-vm.(none)>
2010-04-10 19:02:14 -0400

    Added license.

--- clevercss-mode.el
+++ clevercss-mode.el
@@ -1,8 +1,24 @@
 ;;; clevercss-mode.el --- A major mode for editing CleverCSS files
 
-;; Author: Joe Schafer
+;; Copyright (C) 2010 Joe Schafer
+;; Author: Joe Schafer (joesmoe10@gmail.com)
 ;; Created: Apr 2010
 ;; Keywords: languages css
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 3 of
+;; the License, or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be
+;; useful, but WITHOUT ANY WARRANTY; without even the implied
+;; warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+;; PURPOSE.  See the GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public
+;; License along with this program; if not, write to the Free
+;; Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+;; MA 02111-1307 USA
 
 (defconst clevercss-version "0.1"
   "`clevercss-mode' version number.")
